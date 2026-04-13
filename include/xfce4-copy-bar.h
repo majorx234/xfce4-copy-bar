08f128bbd9c4a430cc82a4ad6d806209f7406ba6
MajorX234 <majorx234@googlemail.com>
2026-04-13 23:56:37 +0200

    Add lib skeleton

---
+++ include/xfce4-copy-bar.h
@@ -0,0 +1,44 @@
+/*
+ *  xfce4-copy-bar
+ *  Copyright (C) 2026  majorx234
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _XFCE4_COPY_BAR_H_
+#define  _XFCE4_COPY_BAR_H_
+
+#include <libxfce4panel/libxfce4panel.h>
+
+G_BEGIN_DECLS
+
+typedef struct XFCE4CopyBarPlugin {
+    XfcePanelPlugin *plugin;
+
+    GtkWidget       *ebox;
+    GtkWidget       *hvbox;
+    GtkWidget       *label;
+
+    /* sample settings */
+    GtkWidget       *settings_dialog;
+    gchar           *setting1;
+    gint             setting2;
+    gboolean         setting3;
+} XFCE4CopyBarPlugin;
+
+void xfce4_copy_bar_save(XfcePanelPlugin *plugin, XFCE4CopyBarPlugin *sample);
+
+G_END_DECLS
+
+#endif // _XFCE4_COPY_BAR_H_
---
+++ src/xfc4-copy-bar.cpp
@@ -0,0 +1,5 @@
+#include "xfce4-copy-bar.h"
+
+void xfce4_copy_bar_save(XfcePanelPlugin *plugin, XFCE4CopyBarPlugin *sample){
+
+}
